255536b193658da0b09c8a8c6951a66761ab6918
kudrykun <kudrykun@gmail.com>
2018-09-05 21:56:39 +0300

    Синглтончик

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour {
 
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroy = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int score = 0;
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        SetScoreText(0);
+    }
+
+    void Update () {
         Time.timeScale = gameSpeed;
 	}
 
     public void addToScore()
     {
         score += pointsPerBlockDestroy;
+        SetScoreText(score);
+    }
+
+    private void SetScoreText(int score)
+    {
+        scoreText.SetText(score.ToString());
     }
 }
